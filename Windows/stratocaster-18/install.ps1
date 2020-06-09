8ad54d8e013e12a89d2d25923d17b10dee055b7a
sata365 <9369075+sata365@users.noreply.github.com>
2020-06-10 00:47:07 +0900

    initial commit

---
+++ Windows/stratocaster-18/install.ps1
@@ -0,0 +1,17 @@
+﻿
+function Invoke-Chocolatey {
+    Import-Csv -LiteralPath "$PSScriptRoot/chocolatey.csv" -Encoding UTF8 | %{Write-Host "Installing: " $_.Name ;Invoke-Expression -Command ('choco install ' + $_.Name + ' -y')} 
+}
+
+function Install-Modules {
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    <#
+    Name                      InstallationPolicy   SourceLocation
+    ----                      ------------------   --------------
+    PSGallery                 Trusted              https://www.powershellgallery.com/api/v2
+    #>
+    Install-Module -Name PowerShellGet, ExchangeOnlineManagement, AzureADPreview
+}
+
+Invoke-Chocolatey
+Install-Modules
