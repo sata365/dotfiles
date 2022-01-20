b27d4c2c5ca61b7d1b2e176c32905b754cfc56b5
sata365 <9369075+sata365@users.noreply.github.com>
2022-01-20 17:34:25 +0900

    update: git clone behaviour

--- macOS/install.sh
+++ macOS/install.sh
@@ -5,17 +5,23 @@ echo "------- Start configuration."
 GIT_BASE_DIR=${GIT_BASE_DIR:=~/git}
 export DOTFILES_REPO_DIR=${GIT_BASE_DIR}/dotfiles
 
-if [ ! -d "${GIT_BASE_DIR}/dotfiles/.git" ]; then
+if [ ! -d "${DOTFILES_REPO_DIR}/.git" ]; then
   git clone https://github.com/sata365/dotfiles.git "${DOTFILES_REPO_DIR}"
 else
   pushd ${DOTFILES_REPO_DIR}
-  git pull origin master
+  git pull origin
   popd
 fi
 
 export LOCAL_DOTFILES_REPO_DIR=${GIT_BASE_DIR}/dotfiles.local
 if [[ ${LOCAL_DOTFILES_REPO:-x} != x ]] ; then
-  git clone "${LOCAL_DOTFILES_REPO}" "${LOCAL_DOTFILES_REPO_DIR}"
+  if [ ! -d "${LOCAL_DOTFILES_REPO_DIR}/.git" ]; then
+    git clone "${LOCAL_DOTFILES_REPO}" "${LOCAL_DOTFILES_REPO_DIR}"
+  else
+    pushd ${LOCAL_DOTFILES_REPO_DIR}
+    git pull origin
+    popd
+  fi
 fi
 
 /bin/bash ${DOTFILES_REPO_DIR}/macOS/.bin/defaults.sh
