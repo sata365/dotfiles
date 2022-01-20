b299d2c63418a3f9db2fda01566bc77a2505be1f
sata365 <9369075+sata365@users.noreply.github.com>
2022-01-20 19:00:13 +0900

    add: brew tap homebrew/cask-drivers

--- macOS/.bin/brew.sh
+++ macOS/.bin/brew.sh
@@ -4,6 +4,7 @@ echo "------- Start Homebrew install."
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew tap homebrew/cask-drivers
 xargs brew install < ${DOTFILES_REPO_DIR}/macOS/.resources/Homebrew/brew.txt
 xargs brew install --cask --force  < ${DOTFILES_REPO_DIR}/macOS/.resources/Homebrew/cask.txt
 
