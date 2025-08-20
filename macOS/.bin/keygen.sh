434ee5ea7e5040449415c6efdde96a7bc47fdfbd
sata365 <9369075+sata365@users.noreply.github.com>
2025-08-20 14:09:28 +0900

    update

--- macOS/.bin/keygen.sh
+++ macOS/.bin/keygen.sh
@@ -3,13 +3,13 @@
 echo "------- Start generate keys."
 
 #gitname=$(git config --get user.name)
-gitemail=$(git config --get user.email)
+#gitemail=$(git config --get user.email)
 
-if [ ! -f ~/.ssh/id_25519 ]; then
-  sshkeypasswd=`RAND=$RANDOM && echo ${RAND} | sha256sum | base64 | head -c 32`
-  ssh-keygen -t ed25519 -C ${gitemail} -N ${sshkeypasswd} -f ~/.ssh/id_ed25519
-  echo "sshpasswd: ${sshkeypasswd}" >> ~/.dotfiles_tmp
-fi
+#if [ ! -f ~/.ssh/id_25519 ]; then
+#  sshkeypasswd=`RAND=$RANDOM && echo ${RAND} | sha256sum | base64 | head -c 32`
+#  ssh-keygen -t ed25519 -C ${gitemail} -N ${sshkeypasswd} -f ~/.ssh/id_ed25519
+#  echo "sshpasswd: ${sshkeypasswd}" >> ~/.dotfiles_tmp
+#fi
 
 #gpgkeypasswd=`RAND=$RANDOM && echo ${RAND} | sha256sum | base64 | head -c 32`
 #gpg --pinentry-mode loopback --passphrase ${gpgkeypasswd} --quick-generate-key "${gitname} <${gitemail}>" future-default default 0
@@ -19,9 +19,9 @@ fi
 #gpgkeyid=$(gpg --list-secret-keys --keyid-format=long ${gitname} | grep "sec" | awk '{print $2}' | sed -E 's_.+/__g')
 #git config --file ~/.gitconfig_device_local user.signingkey ${gpgkeyid}
 
-cat ~/.dotfiles_tmp && ~/.dotfiles_tmp
+#cat ~/.dotfiles_tmp && ~/.dotfiles_tmp
 
-eval "$(ssh-agent -s)"
-ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+#eval "$(ssh-agent -s)"
+#ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 
 echo "------- Finish generate keys."
